01bafef7eaaad361153bc487bbf08a8c12ea153e
Devansh444 <sharma.devansh0905@gmail.com>
2025-08-08 02:46:57 +0530

    train

--- main.py
+++ main.py
@@ -31,13 +31,12 @@ except Exception as e:
 
 
 STAGE_NAME = "Training"
-try: 
-   logger.info(f"*******************")
+try:
+   logger.info("*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   model_trainer = ModelTrainingPipeline()
-   model_trainer.main()
+   ModelTrainingPipeline().main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+   logger.exception(e)
+   raise e
 
--- src/cnnClassifier/pipeline/stage_03_training.py
+++ src/cnnClassifier/pipeline/stage_03_training.py
@@ -3,36 +3,31 @@ from cnnClassifier.components.prepare_callbacks import PrepareCallback
 from cnnClassifier.components.training import Training
 from cnnClassifier import logger
 
-
+STAGE_NAME = "Training"
 
 class ModelTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
-        # Prepare callbacks
-        prepare_callbacks = PrepareCallback(config=config.get_prepare_callback_config())
-        callback_list    = prepare_callbacks.get_tb_ckpt_callbacks()
+        config = ConfigurationManager()
 
-        # Training
+        prepare_callbacks = PrepareCallback(
+            config=config.get_prepare_callback_config()
+        )
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
         training = Training(config=config.get_training_config())
-        training.get_base_model()           # loads & recompiles with run_eagerly=True
-        training.train_valid_generator()    # sets up your ImageDataGenerators
-    
-        training.train(callback_list=callback_list)  # kicks off .fit()
-
-
-
-
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(callback_list=callback_list)
 
 if __name__ == '__main__':
     try:
-        logger.info(f"*******************")
+        logger.info("*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainingPipeline()
-        obj.main()
+        ModelTrainingPipeline().main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
-        
